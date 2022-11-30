4ba915581b7aa24d03909a3917b562ced13d7ea4
Abu-Siam <abu.siam@dsinnovators.com>
2022-11-30 17:53:27 +0600

    fix styling issues with galib

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1600px'
+    },
     extend: {},
   },
   plugins: [],
+
 }
